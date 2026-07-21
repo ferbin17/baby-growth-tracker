cc67178c99b70c32ef6230355e46c58888bd9cc7
ferbin17 <ferbin17@gmail.com>
2026-07-21 10:21:40 +0530

    Added vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full h-full overflow-hidden bg-[radial-gradient(circle_at_top_left,rgba(186,230,253,0.35),transparent_45%),linear-gradient(135deg,#f8fbff_0%,#fdfefe_100%)] text-slate-900">
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
